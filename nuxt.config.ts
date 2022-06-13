471461d6957703e435c1e3c0a34b3a0e2af12a86
Julian Mollik <github@mollik.net>
2022-06-13 13:44:48 +0200

    added postcss config & global css config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    css: ['@/assets/style/global.css'],
+    build: {
+        postcss: {
+            postcssOptions: require('./postcss.config.js'),
+        },
+    }
 })
