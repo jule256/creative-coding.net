48eca641fac139b6252a7cae43ff72a21ee4ca56
Julian Mollik <github@mollik.net>
2022-06-13 22:22:41 +0200

    added basic theme and a - temporary - crazy try-out theme

---
+++ config/themes.js
@@ -0,0 +1,20 @@
+export const themes = {
+    'default': {
+        fontColor: '#000000',
+        borderColor: '#336600',
+        backgroundColor: '#FFFFFF', // @todo ➔ rename to "inner"
+        backgroundColorOuter: '#333333',
+        transparentColor: 'transparent',
+        gradientColorLight: '#427142',
+        gradientColorDark: '#437243',
+    },
+    'crazy': {
+        fontColor: 'rgb(139, 90, 30)',
+        borderColor: 'deeppink',
+        backgroundColor: 'lightblue',
+        backgroundColorOuter: '#333333',
+        transparentColor: 'transparent',
+        gradientColorLight: '#FF0000',
+        gradientColorDark: '#FFFF00',
+    }
+}
