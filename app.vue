44e817196925b0f7d216059b4461225a21d08e1f
Julian Mollik <github@mollik.net>
2022-06-13 22:19:25 +0200

    added primitive theming option

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="container" id="top">
+  <div class="container" id="top" :style="themeVariables">
+    <button class="temp" @click="themeKey = themeKey === 'default' ? 'crazy' : 'default'">changeTheme ({{ themeKey
+    }})</button>
     <Header :title="title" />
     <Navigation />
     <NuxtPage @updateTitle="setTitle" />
@@ -7,6 +9,28 @@
   </div>
 </template>
 <script setup>
+import { themes as themeLibrary } from './config/themes.js'
+
 const title = ref('...')
 const setTitle = newTitle => title.value = newTitle;
+
+const themeKey = ref('default')
+const themeVariables = computed(() => {
+  return {
+    '--font-color': themeLibrary[themeKey.value].fontColor,
+    '--border-color': themeLibrary[themeKey.value].borderColor,
+    '--background-color': themeLibrary[themeKey.value].backgroundColor,
+    '--transparent-color': themeLibrary[themeKey.value].transparentColor,
+    '--gradient-color-light': themeLibrary[themeKey.value].gradientColorLight,
+    '--gradient-color-dark': themeLibrary[themeKey.value].gradientColorDark,
+  }
+})
+
 </script>
+<style lang="postcss" scoped>
+.temp {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+</style>
