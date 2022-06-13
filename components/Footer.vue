4151923ecdd3752fca67fd84b61d19b85d8e4e29
Julian Mollik <github@mollik.net>
2022-06-13 13:46:49 +0200

    restructured styling code (postcss)

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
     <footer class="footer">
-        <div class="footer-content">
+        <div class="content">
             <div class="copyright">
                 &copy; Julian Mollik {{ new Date().getFullYear() }}
             </div>
             <div>
-                <NuxtLink class="link" @click="scrollToTop">top</NuxtLink>
+                <NuxtLink class="emphasized" @click="scrollToTop">top</NuxtLink>
             </div>
             <div>
-                <NuxtLink to="sitemap" class="link">sitemap</NuxtLink>
+                <NuxtLink to="sitemap" class="emphasized">sitemap</NuxtLink>
             </div>
         </div>
     </footer>
@@ -22,7 +22,7 @@ const scrollToTop = () => {
 </script>
 
 
-<style>
+<style lang="postcss" scoped>
 .footer {
     position: relative;
     width: 480px;
@@ -31,54 +31,49 @@ const scrollToTop = () => {
     border-width: 0 3px 3px 3px;
     background-color: #FFFFFF;
     padding: 0px 20px 0px 20px;
-}
 
-.footer::before,
-.footer::after {
-    display: block;
-    width: 90px;
-    height: 28px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    content: '';
-    z-index: 1;
-    background: linear-gradient(193deg, rgba(67, 114, 67, 0) 46%, rgba(67, 114, 67, 0.8477591720281863) 83%, rgba(67, 114, 67, 1) 100%);
-    background-repeat: no-repeat;
-}
+    &::before,
+    &::after {
+        display: block;
+        width: 90px;
+        height: 28px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        content: '';
+        z-index: 1;
+        background: linear-gradient(193deg, rgba(67, 114, 67, 0) 46%, rgba(67, 114, 67, 0.8477591720281863) 83%, rgba(67, 114, 67, 1) 100%);
+        background-repeat: no-repeat;
+    }
 
-.footer::after {
-    z-index: 2;
-    background: repeating-linear-gradient(0deg,
-            #FFFFFF,
-            #FFFFFF 2px,
-            rgba(0, 0, 0, 0) 2px,
-            rgba(0, 0, 0, 0) 4px);
-}
+    &::after {
+        z-index: 2;
+        background: repeating-linear-gradient(0deg,
+                #FFFFFF,
+                #FFFFFF 2px,
+                rgba(0, 0, 0, 0) 2px,
+                rgba(0, 0, 0, 0) 4px);
+    }
 
-.footer-content {
-    position: absolute;
-    left: 80px;
-    top: 3px;
-    z-index: 3;
-    display: flex;
-    justify-content: space-between;
+    .content {
+        position: absolute;
+        width: 440px;
+        left: 80px;
+        top: 3px;
+        z-index: 3;
+        display: flex;
+        justify-content: space-between;
 
-    width: 440px;
-}
+        .copyright,
+        .emphasized {
+            height: 25px;
+            line-height: 28px;
+        }
 
-.copyright,
-.link {
-    height: 25px;
-    line-height: 28px;
-}
-
-.link {
-    text-decoration: none;
-    color: #000000;
-    font-weight: bold;
-    display: block;
-    padding: 0 20px 0 20px;
-    cursor: pointer;
+        .emphasized {
+            display: block;
+            padding: 0 20px 0 20px;
+        }
+    }
 }
 </style>
