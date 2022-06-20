9305dcfa952e151063a75a8c3d101522840d0c0a
Julian Mollik <github@mollik.net>
2022-06-20 18:30:16 +0200

    added hover-color and some refactoring

--- config/themes.js
+++ config/themes.js
@@ -2,19 +2,21 @@ export const themes = {
     'default': {
         fontColor: '#000000',
         borderColor: '#336600',
-        backgroundColor: '#FFFFFF', // @todo ➔ rename to "inner"
+        backgroundColorContent: '#FFFFFF',
         backgroundColorOuter: '#333333',
         transparentColor: 'transparent',
         gradientColorLight: '#427142',
         gradientColorDark: '#437243',
+        hoverColor: '#EEEEEE',
     },
     'crazy': {
         fontColor: 'rgb(139, 90, 30)',
         borderColor: 'deeppink',
-        backgroundColor: 'lightblue',
+        backgroundColorContent: 'lightblue',
         backgroundColorOuter: '#333333',
         transparentColor: 'transparent',
         gradientColorLight: '#FF0000',
         gradientColorDark: '#FFFF00',
+        hoverColor: '#00FFEE',
     }
 }
