287fe771690b8d7bb7c3bd15161e6caf3985845d
Julian Mollik <github@mollik.net>
2022-06-20 18:32:18 +0200

    added sidebar-entry component

---
+++ components/SidebarEntry.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="entry">
+        <h1>{{ headline }}</h1>
+        <div>
+            [ <NuxtLink @mouseover="handleMouseEnter" @mouseleave="handleMouseLeave" @click="handleGoTo">go to
+            </NuxtLink>]
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    headline: {
+        type: String,
+        default: '...'
+    },
+    id: {
+        type: Number,
+        default: 0
+    }
+})
+
+const emit = defineEmits(['goTo', 'mouseEnter', 'mouseLeave']);
+
+const handleGoTo = () => {
+    emit('goTo', props.id)
+}
+
+const handleMouseEnter = () => {
+    emit('mouseEnter', props.id)
+}
+
+const handleMouseLeave = () => {
+    emit('mouseLeave', props.id)
+}
+</script>
+
+<style lang="postcss" scoped>
+.entry {
+    display: grid;
+    grid-template-columns: 130px 42px;
+    gap: 0 10px;
+    margin-bottom: 10px;
+}
+</style>
