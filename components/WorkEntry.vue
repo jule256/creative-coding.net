7baad8dd609f74b0860b7f62faaa6a0807005373
Julian Mollik <github@mollik.net>
2022-06-23 20:54:13 +0200

    added work-entry component

---
+++ components/WorkEntry.vue
@@ -0,0 +1,172 @@
+<template>
+    <article class="entry" :class="{ 'is-highlighted': isHighlighted }" :id="`work-entry-${data.id}`">
+        <div class="header">
+            <h2>{{ data.headline }}</h2>
+            <div class="control">
+                [ <NuxtLink @mouseover="isHover = true" @mouseleave="isHover = false" @click="handleToggle"
+                    :title="toggleTitle">{{ data.isExpanded ?
+                            'hide' : 'show'
+                    }}</NuxtLink> ]
+            </div>
+        </div>
+        <transition name="toggle" @after-enter="handleUpdateHeight" @before-leave="handleUpdateHeight">
+            <div v-if="data.isExpanded" class="story">
+                <div class="wrapper" :class="{ 'reverse': (index % 2 !== 0) }">
+                    <div class="picture">
+                        <img class="framed" :src="imageSrc" :title="data.image[0].title.de"
+                            :alt="data.image[0].alt.de" />
+                    </div>
+                    <div class="text">
+                        <p v-for="paragraph in data.content" v-html="parseInline(paragraph)" />
+                    </div>
+                </div>
+                <ul>
+                    <li v-if="data.keywords">
+                        <h2>Keywords: </h2>
+                        <template v-for="(keyword, index) in data.keywords">
+                            <NuxtLink v-if="keyword.url" :href="keyword.url" :title="keyword.title">
+                                {{ keyword.text }}</NuxtLink>
+                            <span v-else>{{ keyword.text }}</span>
+                            <span v-if="(index < data.keywords.length - 1)">, </span>
+                        </template>
+                    </li>
+                    <li v-for="link in data.links">
+                        <h2>Link: </h2>
+                        <NuxtLink :href="link.url" :title="link.title">{{ link.text }}</NuxtLink>
+                        <span v-if="link.extra" v-html="`&nbsp;${parseInline(link.extra)}`" />
+                    </li>
+                </ul>
+                <p class="footer">
+                    updated {{ data.date }}
+                </p>
+            </div>
+        </transition>
+    </article>
+</template>
+<script setup>
+import { parseInline } from 'marked'
+
+const emit = defineEmits(['status-change', 'update-height'])
+
+const props = defineProps({
+    data: {
+        type: Object,
+        default: () => { }
+    },
+    index: {
+        type: Number,
+        default: 0
+    }
+})
+
+const imageSrc = computed(() => {
+    return `/assets/picture/${props.data.image[0].src}`
+})
+
+const {
+    contentMaxHeight,
+    handleToggle,
+    handleUpdateHeight,
+    isHover,
+    isHighlighted,
+    toggleTitle,
+} = useEntry(props.data, emit)
+</script>
+
+<style lang="postcss" scoped>
+.story {
+    overflow: hidden;
+    max-height: v-bind(contentMaxHeight);
+}
+
+.toggle-enter-active {
+    transition: max-height 1s ease-out;
+}
+
+.toggle-leave-active {
+    transition: max-height 1s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.toggle-enter-from,
+.toggle-leave-to {
+    max-height: 0;
+}
+
+.entry {
+    margin: 0 -6px;
+    padding: 0 6px;
+
+    transition: background-color var(--mouseover-transition-duration) ease-in;
+    border-radius: 4px;
+
+    &.is-highlighted {
+        background-color: var(--hover-color);
+    }
+}
+
+.header {
+    display: grid;
+    grid-template-columns: 370px 100px;
+    gap: 0 12px;
+    height: 26px;
+    line-height: 26px;
+
+    .control {
+        text-align: right;
+    }
+}
+
+.story {
+    margin-bottom: 10px;
+
+    h2 {
+        display: inline;
+    }
+
+    ul {
+        li {
+            position: relative;
+            padding-left: 12px;
+            margin-top: 5px;
+
+            &::before {
+                content: '» ';
+                position: absolute;
+                left: 0;
+            }
+        }
+    }
+
+    p {
+        text-indent: 12px;
+        text-align: justify;
+        margin-bottom: 6px;
+
+        &.footer {
+            text-align: right;
+            margin-top: 10px;
+        }
+    }
+}
+
+.wrapper {
+    display: grid;
+    gap: 0 8px;
+    grid-template-columns: 210px 260px;
+
+    &.reverse {
+        gap: 0 14px;
+        grid-template-columns: 260px 210px;
+
+        .picture {
+            order: 1;
+        }
+    }
+
+    margin-bottom: 10px;
+
+    .picture {
+        width: 200px;
+    }
+}
+</style>
