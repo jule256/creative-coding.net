a6fb468c86b2051a7cd01c5b7944664166ce7101
Julian Mollik <github@mollik.net>
2022-07-10 00:44:40 +0200

    added hateoas-index config

--- config/config.js
+++ config/config.js
@@ -2,3 +2,5 @@ export const DEFAULT_NEWS_STORY_HEIGHT = 270
 export const DEFAULT_WORK_STORY_HEIGHT = 350
 export const OPEN_NEWS_STORIES_BY_DEFAULT = 1
 export const OPEN_WORK_STORIES_BY_DEFAULT = 3
+
+export const HATEOAS_INDEX = 'http://api.creative-coding.net/'
