08e7d25d1985895c1d8ce8babc26c1abc46b55b7
Julian Mollik <github@mollik.net>
2022-07-10 23:50:58 +0200

    added date-formatting helper

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,3 +2,8 @@ export const isElementInViewport = (el, expandedHeight) => {
     const lowerEnd = el.offsetTop + el.offsetHeight + expandedHeight
     return lowerEnd < window.scrollY + window.innerHeight && lowerEnd > window.scrollY
 }
+
+export const formatDate = isoDate => {
+    const d = new Date(isoDate)
+    return d.toLocaleDateString('en-GB')
+}
