9e489a90e706ba1848905cfa157998b32f4ea58c
Julian Mollik <github@mollik.net>
2022-07-10 17:08:05 +0200

    implemented primitive loading-mechanism

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,18 @@
 <template>
     <aside class="sidebar">
-        <slot></slot>
+        <div v-if="isLoading" style="text-align: center; padding: 30px;">
+            ... is loading ... @todo
+        </div>
+        <template v-else>
+            <slot></slot>
+        </template>
     </aside>
 </template>
+<script setup>
+const props = defineProps({
+    isLoading: {
+        type: Boolean,
+        default: false
+    },
+})
+</script>
