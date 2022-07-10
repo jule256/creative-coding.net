8f8c939b557d96bbc984c28ae306a8c17a1bc14e
Julian Mollik <github@mollik.net>
2022-07-10 23:52:50 +0200

    now fetching work-entry data via API

--- pages/work.vue
+++ pages/work.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="content">
         <main class="main">
-            <WorkEntry v-for="(WorkEntry, index) in workList" :data="WorkEntry" :index="index"
+            <div v-if="queryStatus.isLoading" style="text-align: center; padding: 30px;">
+                ... is loading ... @todo
+            </div>
+            <WorkEntry v-else v-for="(WorkEntry, index) in workList" :data="WorkEntry" :index="index"
                 @status-change="handleStatusChange" @update-height="handleHeightUpdate" />
         </main>
-        <Sidebar class="sidebar" name="work">
+        <Sidebar :is-loading="queryStatus.isLoading" class="sidebar">
             <SidebarEntry v-for="workEntry in workList" :headline="workEntry.headline" :id="workEntry.id"
                 @go-to="handleGoTo" @mouse-enter="enableHighlight" @mouse-leave="disableHighlight" />
         </Sidebar>
@@ -14,6 +17,9 @@
 <script setup>
 import { work as workListRaw } from '../api/work.js'
 import { DEFAULT_WORK_STORY_HEIGHT, OPEN_WORK_STORIES_BY_DEFAULT } from '../config/config.js'
+import { useQuery } from 'vue-query'
+import { fetchData } from '../helpers/network'
+const emit = defineEmits(['updateTitle'])
 
 const workList = ref([])
 
@@ -25,17 +31,64 @@ const {
     handleGoTo
 } = useEntryList(workList, 'work')
 
-const emit = defineEmits(['updateTitle'])
+const {
+    isHateoasLoading,
+    isHateoasError,
+    hateoasError,
+    getUrl,
+} = useHateoas()
+
+const workQuery = reactive(useQuery(
+    ['work'],
+    () => fetchData(getUrl('work')),
+    { staleTime: 10000000 }
+))
+
+const queryStatus = reactive({
+    isLoading: computed(
+        () => workQuery.isLoading || isHateoasLoading.value
+    ),
+    isError: computed(
+        () => workQuery.isError || isHateoasError.value
+    ),
+    error: computed(
+        () => queryStatus.isError ? (workQuery.error || hateoasError.value) : ''
+    ),
+    workList: computed(
+        () => workQuery && workQuery.data
+    ),
+})
+
+const enrichWorkList = queryStatus => {
+    if (queryStatus.workList?.data) {
+        return toRaw(queryStatus.workList.data)
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+            .map((workListEntry, index) => {
+                workListEntry.isHighlighted = false
+                workListEntry.isExpanded = index < OPEN_WORK_STORIES_BY_DEFAULT
+                workListEntry.height = DEFAULT_WORK_STORY_HEIGHT
+                return workListEntry
+            })
+    } else {
+        return []
+    }
+}
+
+watch(
+    () => queryStatus.workList?.data.length || 0,
+    (newLength, oldLength) => {
+        if (newLength !== oldLength) {
+            // only enrich newslist with the status-values if the data was freshly fetched
+            workList.value = enrichWorkList(queryStatus)
+        }
+    }
+)
+
 
 onMounted(() => {
     emit('updateTitle', 'work')
 
-    workList.value = workListRaw.reverse().map((workListEntry, index) => {
-        workListEntry.isHighlighted = false
-        workListEntry.isExpanded = index < OPEN_WORK_STORIES_BY_DEFAULT
-        workListEntry.height = DEFAULT_WORK_STORY_HEIGHT
-        return workListEntry
-    })
+    workList.value = enrichWorkList(queryStatus)
 })
 </script>
 <style lang="postcss" scoped>
