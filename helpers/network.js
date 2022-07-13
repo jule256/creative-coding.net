cf746e046d268d7f95ace7f3c09f772a10f73da5
Julian Mollik <github@mollik.net>
2022-07-14 00:45:55 +0200

    improved error logging

--- helpers/network.js
+++ helpers/network.js
@@ -12,10 +12,10 @@ const fetchWrapper = (url, options = {}) => {
 
 export const getIndex = async (options = {}) => {
     return await fetchWrapper(HATEOAS_INDEX, options)
-        .catch(e => console.log(`Error fetching index from ${HATEOAS_INDEX}:`, e))
+        .catch(e => console.log(`Error fetching index from '${HATEOAS_INDEX}':`, e))
 }
 
 export const fetchData = async (url, options = {}) => {
     return await fetchWrapper(url, options)
-        .catch(e => console.log(`Error fetching data from ${url}:`, e))
+        .catch(e => console.log(`Error fetching data from '${url}':`, e))
 }
