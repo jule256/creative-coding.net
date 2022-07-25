4bbc126f3202bc442b0473920e98e4b50fc81051
Julian Mollik <github@mollik.net>
2022-07-25 16:24:41 +0200

    added re-usable config with section-info

---
+++ config/sections.js
@@ -0,0 +1,31 @@
+
+export const SECTIONS = [
+    {
+        id: 'home',
+        title: {
+            en: 'home'
+        },
+        to: '/'
+    },
+    {
+        id: 'work',
+        title: {
+            en: 'work'
+        },
+        to: 'work'
+    },
+    {
+        id: 'cv',
+        title: {
+            en: 'cv'
+        },
+        to: 'cv'
+    },
+    {
+        id: 'contact',
+        title: {
+            en: 'contact'
+        },
+        to: 'contact'
+    },
+]
