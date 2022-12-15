214a95ca26963e444ecc77cfe57450a82158b6de
Julian Mollik <julian.mollik@sanitas.com>
2022-12-15 14:15:09 +0100

    moved post-css config to nuxt.config .ts file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,15 @@
-import { defineNuxtConfig } from 'nuxt'
+// import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: [
         '@/assets/style/reset.css',
         '@/assets/style/variables.css',
-        '@/assets/style/global.css'
+        '@/assets/style/global.css',
     ],
-    build: {
-        postcss: {
-            postcssOptions: require('./postcss.config.js'),
-        },
+    postcss: {
+        plugins: {
+            'postcss-nested': {},
+        }
     }
 })
 
