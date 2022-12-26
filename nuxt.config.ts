acadb12dc61e64ce14a9ab88579a09f2d5ae2b37
Julian Mollik <github@mollik.net>
2022-12-26 22:03:44 +0100

    added static/default head-properties (WIP)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,23 @@ export default defineNuxtConfig({
         plugins: {
             'postcss-nested': {},
         }
+    },
+    app: {
+        head: {
+            title: "Julian Mollik",
+            meta: [
+                { charset: "utf-8" },
+                { name: "viewport", content: "width=device-width, initial-scale=1" },
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "description content"
+                }
+            ],
+            link: [
+                { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            ]
+        },
     }
 })
 
