3149a3cc57e7386c3fb6ce0b6a476a11ca16908b
Julian Mollik <github@mollik.net>
2022-12-27 15:04:49 +0100

    now handling dynamic favicon inline

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
 
     <Head>
       <Title>{{ titleHead }}</Title>
+      <Link rel="icon" :href="`data:image/svg+xml,${faviconData}`" type="image/svg+xml" />
     </Head>
     <component :is="'style'">
       :root {
@@ -31,35 +32,23 @@ const isFocused = ref(true)
 const title = ref('')
 const titleHead = ref('')
 
-const faviconPath = computed(() => {
-  const mapping = {
-    default: {
-      focus: 'default_focus.svg',
-      blur: 'default_blur.svg',
-    },
-    crazy: {
-      focus: 'crazy_focus.svg',
-      blur: 'crazy_blur.svg',
-    }
-  }
-  return `favicon/${mapping[themeKey.value][isFocused.value ? 'focus' : 'blur']}`
-})
-
-useHead({
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: faviconPath
-    }
-  ]
-})
-
 const setTitles = id => {
   title.value = PAGES.find(page => page.id === id).title.header['en']
   titleHead.value = setHeadTitle(id)
 }
 
+// created with https://yoksel.github.io/url-encoder/
+const faviconData = computed(() => {
+  return [
+    `%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E`,
+    `%3Csvg viewBox='-0.001 0 100 85' width='100' height='85' xmlns='http://www.w3.org/2000/svg'%3E`,
+    `  %3Cg transform='matrix(3.13, 0, 0, 3.13, 0, 0)'%3E`,
+    `    %3Cpath d='M 15.988 -2.489 L 29.488 5.507 L 29.488 21.496 L 15.988 29.488 L 2.488 21.496 L 2.488 5.507 L 15.988 -2.489 Z' style='fill: %23${themeLibrary[themeKey.value].favicon[isFocused.value ? 'focus' : 'blur'].faviconColorFill1};' transform='matrix(0, 1, -1, 0, 30, -2.5)'/%3E`,
+    `    %3Cpath d='M 15.988 0.867 L 26.655 7.185 L 26.655 19.817 L 15.988 26.132 L 5.321 19.817 L 5.321 7.185 L 15.988 0.867 Z' style='fill: %23${themeLibrary[themeKey.value].favicon[isFocused.value ? 'focus' : 'blur'].faviconColorFill2}; stroke: %23${themeLibrary[themeKey.value].favicon[isFocused.value ? 'focus' : 'blur'].faviconColorStroke2}; stroke-width: 2px;' transform='matrix(0, 1, -1, 0, 30, -2.5)'/%3E`,
+    `  %3C/g%3E`,
+    `%3C/svg%3E`,
+  ].join('')
+})
 
 const appVariables = computed(() => ({
   '--font-color': themeLibrary[themeKey.value].fontColor,
