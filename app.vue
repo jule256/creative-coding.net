fdca659d6c8371ef8892af5393862fd1108efac7
Julian Mollik <github@mollik.net>
2022-12-28 21:26:08 +0100

    extracted title and favicon functionality into composable

--- app.vue
+++ app.vue
@@ -19,51 +19,24 @@
   </div>
 </template>
 <script setup>
-import { PAGES } from '@/config/pages'
-import { setHeadTitle, getPageTitle, getSvg } from '@/helpers/helpers'
 import { themes as themeLibrary } from '@/config/themes'
-
-const BLURRED_TITLES = [
-  `Julian Mollik » __PAGE_TITLE__`,
-  `Julian Mollik  »__PAGE_TITLE__`,
-  `Julian Mollik  «__PAGE_TITLE__`,
-  `Julian Mollik « __PAGE_TITLE__`,
-  `Julian Mollik«  __PAGE_TITLE__`,
-  `Julian Mollik»  __PAGE_TITLE__`,
-]
 
 const route = useRoute()
 
 const {
   changeTheme,
-  themeKey,
+  themeKey
 } = useTheme()
 
-const isFocused = ref(true)
-const title = ref('')
-const titleHead = ref('')
-const pageId = ref('')
-
-let intervalId
-let intervalCount = 0
+const {
+  faviconData,
+  pageId,
+  setTitles,
+  title,
+  titleHead
+} = useTitleAndFavicon(themeKey)
 
 const setPageId = id => pageId.value = id
-
-const setTitles = id => {
-  title.value = PAGES.find(page => page.id === id).title.header['en']
-  titleHead.value = setHeadTitle(id)
-}
-
-const faviconData = computed(() => {
-  const statusKey = isFocused.value ? 'focus' : 'blur'
-  return getSvg(
-    themeLibrary[themeKey.value].favicon[statusKey].fill1,
-    themeLibrary[themeKey.value].favicon[statusKey].fill2,
-    themeLibrary[themeKey.value].favicon[statusKey].stroke2,
-    true,
-    true
-  )
-})
 
 const appVariables = computed(() => ({
   '--font-color': themeLibrary[themeKey.value].fontColor,
@@ -95,31 +68,6 @@ const rootVariables = computed(() => ({
 if (route.fullPath === '/') {
   await navigateTo('/home', { redirectCode: 301 })
 }
-
-onMounted(() => {
-  window.onfocus = () => {
-    isFocused.value = true
-  }
-  window.onblur = () => {
-    isFocused.value = false
-  }
-})
-
-const updateBlurredTitle = () => {
-  const titleIndex = intervalCount % BLURRED_TITLES.length
-  titleHead.value = BLURRED_TITLES[titleIndex].replace('__PAGE_TITLE__', getPageTitle(pageId.value))
-  intervalCount++
-}
-
-watch(isFocused, (isFocused) => {
-  if (isFocused) {
-    clearInterval(intervalId)
-    setTitles(pageId.value)
-  } else {
-    intervalCount = 0
-    intervalId = setInterval(updateBlurredTitle, 1000)
-  }
-})
 
 watch(pageId, (pageId) => {
   setTitles(pageId)
