8b8b3cf76ec7149755970d26921ae09cb9442ca1
Julian Mollik <github@mollik.net>
2022-12-28 14:10:40 +0100

    moved getSvg() to helpers.js

--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -11,6 +11,7 @@
 </template>
 <script setup>
 import { themes as themeLibrary } from '@/config/themes'
+import { getSvg } from '@/helpers/helpers'
 
 const props = defineProps({
     currentTheme: {
@@ -22,22 +23,6 @@ const props = defineProps({
 const emit = defineEmits(['changeTheme'])
 
 const containerWidth = ref(`${(Object.keys(themeLibrary).length * 40) + 20}px`)
-
-const getSvg = (fill1, fill2, stroke2) => {
-    return `
-        <svg viewBox="-0.001 0 100 84.436" width="100" height="84.436" xmlns="http://www.w3.org/2000/svg">
-            <g transform="matrix(3.13, 0, 0, 3.13, 0.001067, 0)">
-                <path
-                    d="M 15.988 -2.489 L 29.488 5.507 L 29.488 21.496 L 15.988 29.488 L 2.488 21.496 L 2.488 5.507 L 15.988 -2.489 Z"
-                    style="fill: #__FILL_1__" transform="matrix(0, 1, -1, 0, 30, -2.5)" />
-                <path
-                    d="M 15.988 0.867 L 26.655 7.185 L 26.655 19.817 L 15.988 26.132 L 5.321 19.817 L 5.321 7.185 L 15.988 0.867 Z"
-                    style="fill: #__FILL_2__; stroke: #__STROKE_2__; stroke-width: 2px;"
-                    transform="matrix(0, 1, -1, 0, 30, -2.5)" />
-            </g>
-        </svg>`
-        .replace('__FILL_1__', fill1).replace('__FILL_2__', fill2).replace('__STROKE_2__', stroke2)
-}
 
 const handleThemeChange = name => {
     emit('changeTheme', name)
