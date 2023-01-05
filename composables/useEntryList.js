042a864f9be6c7d2420ecc1134a18db87ea24e41
Julian Mollik <github@mollik.net>
2023-01-05 20:38:37 +0100

    fixed bug for closed-by-default entries

--- composables/useEntryList.js
+++ composables/useEntryList.js
@@ -45,7 +45,17 @@ export const useEntryList = (list, type) => {
     }
 
     const expandDefaults = type => {
-        const entryIndices = [...Array(ENTRY_CONFIG[`OPEN_${type.toUpperCase()}_ENTRIES_BY_DEFAULT`]).keys()].slice(0, list.value.length)
+        const closedByDefaultIndices = list.value.reduce((result, entry, index) => {
+            entry.isClosedByDefault ? result.push(index) : null
+            return result
+        }, [])
+        const entryIndices =
+            [...Array(ENTRY_CONFIG[`OPEN_${type.toUpperCase()}_ENTRIES_BY_DEFAULT`]).keys()]
+                .slice(0, list.value.length)
+                .reduce((result, entry) => {
+                    closedByDefaultIndices.includes(entry) ? null : result.push(entry)
+                    return result
+                }, [])
         expandByIndex(entryIndices)
     }
 
